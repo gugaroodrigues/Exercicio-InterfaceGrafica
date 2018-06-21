f350a40893d1c50b00b5b4ea3f93d2a78c8fa4ab
Gustavo Rodrigues <gugaaroodrigues@gmail.com>
2018-06-21 09:44:46 -0300

    Finalização da parte grafica

--- ExercicioInterfaceGrafica01/src/Exercicio01/Exercicio01.java
+++ ExercicioInterfaceGrafica01/src/Exercicio01/Exercicio01.java
@@ -1,6 +1,9 @@
 package Exercicio01;
-  
+
+import java.awt.Color;
 import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -8,9 +11,12 @@ import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+
 /**
  *
  * @author Gustavo
@@ -19,7 +25,7 @@ public class Exercicio01 implements BaseExercicio01 {
 
     private JFrame jFrame;
     private JTextField jTextFieldNome, jTextFieldIdade,
-            jTextFielApelido, jTextFielPreco;
+            jTextFieldApelido, jTextFieldPreco;
     private JComboBox jComboBoxRaca;
     private JRadioButton jRadioButtonVivo, jRadioButtonRacao, jRadioButtonMorto,
             jRadioButtonCome;
@@ -28,8 +34,10 @@ public class Exercicio01 implements BaseExercicio01 {
     private JLabel jLabelNome, jLabelIdade, jLabelApelido, jLabelPreco,
             jLabelRaca, jLabelDescricao;
     private JTextArea jTextAreaDescricao;
+
+    private JButton jButtonSalvar, jButtonCancelar, jButtonSobre;
     
-    private JButton jButtonSalvar, jButtonCancelar;
+    private JScrollPane jScrollPaneDiscricao;
 
     public Exercicio01() {
         gerarTela();
@@ -38,6 +46,10 @@ public class Exercicio01 implements BaseExercicio01 {
         gerarDimensoes();
         gerarLocalizacao();
         configurarJCombox();
+        AcaoJButton();
+        estilizarRadioButton();
+        estilizarComponentes();
+        configurarJScrollPane();
         jFrame.setVisible(true);
 
     }
@@ -50,8 +62,8 @@ public class Exercicio01 implements BaseExercicio01 {
         jFrame.setLayout(null);
         jFrame.setLocationRelativeTo(null);
         jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        
-        
+        jFrame.getContentPane().setBackground(
+                Color.decode("#c9cfdb"));
 
     }
 
@@ -59,135 +71,142 @@ public class Exercicio01 implements BaseExercicio01 {
     public void adicionarComponentes() {
         jFrame.add(jTextFieldNome);
         jFrame.add(jTextFieldIdade);
-        jFrame.add(jTextFielPreco);
-        jFrame.add(jTextFielApelido);
-        
+        jFrame.add(jTextFieldPreco);
+        jFrame.add(jTextFieldApelido);
+
         jFrame.add(jLabelNome);
         jFrame.add(jLabelIdade);
         jFrame.add(jLabelRaca);
         jFrame.add(jLabelApelido);
         jFrame.add(jLabelPreco);
         jFrame.add(jLabelDescricao);
-        
+
         jFrame.add(jRadioButtonVivo);
         jFrame.add(jRadioButtonMorto);
         jFrame.add(jRadioButtonRacao);
         jFrame.add(jRadioButtonCome);
 
         jFrame.add(jComboBoxRaca);
-        
+
         jFrame.add(jCheckBoxAdestrado);
         jFrame.add(jCheckBoxCastrado);
         jFrame.add(jCheckBoxVacinado);
         jFrame.add(jCheckBoxTemPedigri);
-        
+
         jFrame.add(jTextAreaDescricao);
-        
+
         jFrame.add(jButtonSalvar);
         jFrame.add(jButtonCancelar);
+        jFrame.add(jButtonSobre);
+        jFrame.add(jScrollPaneDiscricao);
     }
 
-    
     @Override
     public void instanciarComponentes() {
         jTextFieldNome = new JTextField();
         jTextFieldIdade = new JTextField();
-        jTextFielPreco = new JTextField();
-        jTextFielApelido = new JTextField();
-        
+        jTextFieldPreco = new JTextField();
+        jTextFieldApelido = new JTextField();
+
         jLabelNome = new JLabel("Nome");
         jLabelIdade = new JLabel("Idade");
         jLabelRaca = new JLabel("Raça");
         jLabelApelido = new JLabel("Apelido");
         jLabelPreco = new JLabel("Preço");
         jLabelDescricao = new JLabel("Descrição");
-        
+
         jRadioButtonVivo = new JRadioButton("Vivo");
         jRadioButtonMorto = new JRadioButton("Morto");
         jRadioButtonRacao = new JRadioButton("Ração");
         jRadioButtonCome = new JRadioButton("Come almoços, janta");
-        
+
         jCheckBoxAdestrado = new JCheckBox("Adestrado");
         jCheckBoxCastrado = new JCheckBox("Castrado");
         jCheckBoxVacinado = new JCheckBox("Vacinado");
         jCheckBoxTemPedigri = new JCheckBox("Tem pedigree?");
-        
+
         jComboBoxRaca = new JComboBox();
-        
+
         jTextAreaDescricao = new JTextArea();
-        
+
         jButtonSalvar = new JButton("Salvar");
         jButtonCancelar = new JButton("Cancelar");
+        jButtonSobre = new JButton("Sobre");
         
+        jScrollPaneDiscricao = new JScrollPane();
+
     }
 
     @Override
     public void gerarLocalizacao() {
         jTextFieldNome.setLocation(11, 30);
         jTextFieldIdade.setLocation(419, 30);
-        jTextFielApelido.setLocation(195, 88 );
-        jTextFielPreco.setLocation(419, 88);
-        
+        jTextFieldApelido.setLocation(195, 88);
+        jTextFieldPreco.setLocation(419, 88);
+
         jLabelNome.setLocation(11, 5);
         jLabelIdade.setLocation(419, 5);
         jLabelRaca.setLocation(11, 60);
         jLabelApelido.setLocation(195, 60);
         jLabelPreco.setLocation(419, 60);
         jLabelDescricao.setLocation(11, 183);
-        
-        jRadioButtonVivo.setLocation(11 , 125);
+
+        jRadioButtonVivo.setLocation(11, 125);
         jRadioButtonMorto.setLocation(11, 157);
         jRadioButtonRacao.setLocation(110, 125);
         jRadioButtonCome.setLocation(110, 157);
-        
-        jCheckBoxAdestrado.setLocation(360, 125 );
-        jCheckBoxCastrado.setLocation(465, 125 );
-        jCheckBoxVacinado.setLocation(360, 157 );
-        jCheckBoxTemPedigri.setLocation(465, 157 );
-        
+
+        jCheckBoxAdestrado.setLocation(360, 125);
+        jCheckBoxCastrado.setLocation(465, 125);
+        jCheckBoxVacinado.setLocation(360, 157);
+        jCheckBoxTemPedigri.setLocation(465, 157);
+
         jComboBoxRaca.setLocation(11, 88);
-        
-        jTextAreaDescricao.setLocation(11, 211);
-        
-        jButtonSalvar.setLocation(473, 319 );
+
+        //jTextAreaDescricao.setLocation(11, 211);
+        jScrollPaneDiscricao.setLocation(11, 211);       
+
+        jButtonSalvar.setLocation(473, 319);
         jButtonCancelar.setLocation(315, 319);
+        jButtonSobre.setLocation(9, 375);
     }
 
     @Override
     public void gerarDimensoes() {
-        jTextFieldNome.setSize(368, 24 );
+        jTextFieldNome.setSize(368, 24);
         jTextFieldIdade.setSize(204, 24);
-        jTextFielApelido.setSize(184, 24);
-        jTextFielPreco.setSize(204, 24);
-        
+        jTextFieldApelido.setSize(184, 24);
+        jTextFieldPreco.setSize(204, 24);
+
         jLabelNome.setSize(60, 20);
         jLabelIdade.setSize(60, 20);
         jLabelRaca.setSize(60, 20);
         jLabelApelido.setSize(60, 20);
         jLabelPreco.setSize(60, 20);
         jLabelDescricao.setSize(60, 20);
-        
-        jRadioButtonVivo.setSize(100, 20 );
-        jRadioButtonMorto.setSize(100, 20 );
-        jRadioButtonRacao.setSize(100, 20 );
-        jRadioButtonCome.setSize(150, 20 );
-        
-        jCheckBoxAdestrado.setSize(100, 20 );
-        jCheckBoxCastrado.setSize(100, 20 );
-        jCheckBoxVacinado.setSize(100, 20 );
-        jCheckBoxTemPedigri.setSize(150, 20 );
-   
+
+        jRadioButtonVivo.setSize(100, 20);
+        jRadioButtonMorto.setSize(100, 20);
+        jRadioButtonRacao.setSize(100, 20);
+        jRadioButtonCome.setSize(150, 20);
+
+        jCheckBoxAdestrado.setSize(100, 20);
+        jCheckBoxCastrado.setSize(100, 20);
+        jCheckBoxVacinado.setSize(100, 20);
+        jCheckBoxTemPedigri.setSize(150, 20);
+
         jComboBoxRaca.setSize(150, 20);
-        
-        jTextAreaDescricao.setSize(610, 95);
-        
+
+        //jTextAreaDescricao.setSize(610, 95);
+        jScrollPaneDiscricao.setSize(610, 95);
+
         jButtonSalvar.setSize(150, 78);
         jButtonCancelar.setSize(150, 78);
+        jButtonSobre.setSize(70, 20);
     }
-    
-    public void configurarJCombox(){
-        
-        
+
+    public void configurarJCombox() {
+
         DefaultComboBoxModel racas = new DefaultComboBoxModel(
                 new Object[]{
                     "Pastor-alemão", "Labrador retriever", "Buldogue", "Poodle",
@@ -196,19 +215,78 @@ public class Exercicio01 implements BaseExercicio01 {
                 }
         );
         jComboBoxRaca.setModel(racas);
-        jComboBoxRaca.setSelectedIndex(-1);   
+        jComboBoxRaca.setSelectedIndex(-1);
+    }
+
+    public void AcaoJButton() {
+        jButtonSalvar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!jCheckBoxAdestrado.isSelected()
+                        && !jCheckBoxCastrado.isSelected()
+                        && !jCheckBoxVacinado.isSelected()
+                        && !jCheckBoxTemPedigri.isSelected()) {
+                    JOptionPane.showMessageDialog(null,
+                            "Alguma das opções não foram definidas");
+                    return;
+                }
+              
+            }
+        });
     }
     
-    public void AcaoJButton(){
+    private void estilizarRadioButton() {
+        jRadioButtonVivo.setContentAreaFilled(false);
+        jRadioButtonMorto.setContentAreaFilled(false);
+        jRadioButtonRacao.setContentAreaFilled(false);
+        jRadioButtonCome.setContentAreaFilled(false);
         
+        jCheckBoxAdestrado.setContentAreaFilled(false);
+        jCheckBoxCastrado.setContentAreaFilled(false);
+        jCheckBoxTemPedigri.setContentAreaFilled(false);
+        jCheckBoxVacinado.setContentAreaFilled(false);
         
+               
+    }
+    
+    private void estilizarComponentes(){
         
+        jTextFieldNome.setForeground(Color.decode("#0f0f0f"));
+        jTextFieldNome.setBackground(Color.decode("#eff1f4"));
+        jTextFieldIdade.setForeground(Color.decode("#0f0f0f"));
+        jTextFieldIdade.setBackground(Color.decode("#eff1f4"));
+        jTextFieldPreco.setForeground(Color.decode("#0f0f0f"));
+        jTextFieldPreco.setBackground(Color.decode("#eff1f4"));
+        jTextFieldApelido.setForeground(Color.decode("#0f0f0f"));
+        jTextFieldApelido.setBackground(Color.decode("#eff1f4"));
+        
+        jTextAreaDescricao.setForeground(Color.decode("#0f0f0f"));
+        jTextAreaDescricao.setBackground(Color.decode("#eff1f4"));
+        
+        jComboBoxRaca.setForeground(Color.decode("#0f0f0f"));
+        jComboBoxRaca.setBackground(Color.decode("#eff1f4"));
+        
+        jButtonSobre.setBackground(Color.decode("#c9cfdb"));
+     
+    }
+    
+    private void configurarJScrollPane() {
+        jScrollPaneDiscricao.setViewportView(jTextAreaDescricao);
+        jScrollPaneDiscricao.setHorizontalScrollBarPolicy(
+          JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        jScrollPaneDiscricao.setVerticalScrollBarPolicy(
+          JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        jTextAreaDescricao.setLineWrap(true);
     }
     
     
-
-    
-    
-    
-
 }
+
+    
+    
+    
+    
+    
+
+
+
